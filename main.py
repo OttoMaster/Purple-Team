69891480779ae140e2d66a4296623e760600b458
J-hub-star <60904565+J-hub-star@users.noreply.github.com>
2020-03-26 16:50:34 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask_mysql import MySQL
+
+app = Flask(__main__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root12345!'
+app.config['MYSQL_DB'] = 'Fed_Insurance'
+
+mysql = MySQL()
+
+mysql.init__app(app)
+
+@app.route('/')
+def homepage():
+	return render_template('index.html')
+
+
+@app.route('/DirectorOne')
+def DirectorPage:
+	return render_template('DirectorPage.html')
+
+
+@app.route('/DirectorTwo')
+def PageTwo():
+	return render_template('DirectorTwo')
+
+@app.route('/report',methods=['GET','POST'])
+def Report():
+	if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO Employee(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+	return render_template('Report.html')
+
+
+
+
+
+if __name__=='__main:
+	app.run(debug=true)
